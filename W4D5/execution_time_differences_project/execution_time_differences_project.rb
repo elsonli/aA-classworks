5a4f41488f14627a4637b7da225e8bd92b80a402
Elson Li <elson.li@berkeley.edu>
2019-11-08 23:30:11 -0800

    Refactor execution time differences project

--- W4D5/execution_time_differences_project/execution_time_differences_project.rb
+++ W4D5/execution_time_differences_project/execution_time_differences_project.rb
@@ -1,119 +1,72 @@
-require "byebug"
-
-##PHASE I
-
-def my_min(list)
-  list.each do |el_1|
-    return el_1 if list.all? { |el_2| el_1 <= el_2 }
+# First, write a function that compares each element to every other element of
+# the list. Return the element if all other elements in the array are larger.
+# What is the time complexity for this function?
+def my_min(arr)
+  arr.each do |ele_1|
+    return ele_1 if arr.all? { |ele_2| ele_1 <= ele_2 }
   end
 end
-
-## O(n^2)
-
-##PHASE II
-
-def my_min_2(list)
-  smallest = list[0]
-  list.drop(1).each do |el_1|
-    smallest = el_1 if el_1 < smallest 
-  end
-  smallest
-end
-
-## O(n)
 
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)  # =>  -5
+# Runtime Complexity: O(n^2)
+
+# Now rewrite the function to iterate through the list just once while keeping
+# track of the minimum. What is the time complexity?
+def my_min_2(arr)
+  smallest = arr.first
+  arr.drop(1).each { |ele| smallest = ele if ele < smallest }
+  smallest
+end
+
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min_2(list)  # =>  -5
+# Runtime Complexity: O(n)
 
-## PHASE I
+# You have an array of integers and you want to find the largest contiguous
+# (together in sequence) sub-sum. Find the sums of all contiguous sub-arrays
+# and return the max.
 
-def sub_sum(arr)
+# Write a function that iterates through the array and finds all sub-arrays
+# using nested loops. First make an array to hold all sub-arrays. Then find
+# the sums of each sub-array and return the max.
+# Discuss the time complexity of this solution.
+
+def largest_contiguous_subsum(arr)
   subs = []
-  arr.each_with_index do |el1, i1|
-    arr.each_with_index do |el2, i2|
-      subs << arr[i1..i2] if i2 >= i1
+  (0...arr.length).each do |idx_1|
+    (0...arr.length).each do |idx_2|
+      subs << arr[idx_1..idx_2] if idx_2 >= idx_1
     end
   end
   subs.map(&:sum).max
 end
 
-# def better_sub_sum(arr)
-#   max_sum = arr.first
-#   curr_sum = 0
-#   i = 1
-#   while i < arr.length
-#     # debugger
-#     next_ele = arr[i]
-#     curr_sum += next_ele
-#     # check 1: is entire streak > than curr max
-#     # debugger
-#     if max_sum + curr_sum >= max_sum
-#       max_sum += curr_sum
-#       curr_sum = 0
-#     else  
-#       curr_sum = 0
-#     end
-#     if next_ele > max_sum
-#       max_sum = next_ele
-#       curr_sum = 0
-#     end
-#     i += 1
-#   end
-#   max_sum
-# end
+# arr_1 = [5, 3, -7] # 8
+# arr_2 = [2, 3, -6, 7, -6, 7, -9, -2, 5, 10] # 15
+# arr_2 = [-5, -1, -3] # -1
+# p largest_contiguous_subsum(arr_1)
+# p largest_contiguous_subsum(arr_2)
+# p largest_contiguous_subsum(arr_2)
+# Runtime Complexity: O(n^3)
 
-## couldn't use array, because that increased space complexity to O(n), and 
-## array.sum increased time complexity to O(n^2). 
-
-def better_sub_sum(arr)
-  max = arr.first
+# Let's make a better version. Write a new function using O(n) time with O(1)
+# memory. Keep a running tally of the largest sum.
+def largest_contiguous_subsum_2(arr)
+  max_seen = arr.first
   curr_sum = arr.first
-  i = 1
-  while i < arr.length
-    # debugger
-    next_ele = arr[i]
-    if next_ele > max
-      max = next_ele
-    end
-
-    curr_sum += next_ele
-    if curr_sum >= 0
-      if curr_sum > max
-        max = curr_sum
-      end
-    else
-      curr_sum = 0
-    end
-    i += 1
+  arr.drop(1).each do |curr_ele|
+    max_seen = curr_ele if max_seen < curr_ele
+    curr_sum += curr_ele
+    max_seen = curr_sum if max_seen < curr_sum
+    curr_sum = 0 if curr_sum < 0
   end
-  max
+  max_seen
 end
 
-list1 = [5, 3, -7]
-list2 = [2, 3, -6, 7, -6, 7, -9, -2, 5, 10] # 15
-list3 = [-5, -1, -3, 5]
-# p sub_sum(list)
-# p better_sub_sum(list1)
-# p better_sub_sum(list2)
-# p better_sub_sum(list3)
-
-def first_anagram?(str_1, str_2)
-  perms = str_1.chars.permutation.to_a
-  perms.map! { |perm| perm.join("") }
-  perms.include?(str_2)
-end
-
-# p first_anagram?("gizmo", "sally")    #=> false
-# p first_anagram?("elvis", "lives")    #=> true
-
-def second_anagram?(str_1, str_2)
-  str_1.each_char do |char|
-    return false if !str_1.include?(str_2[0])
-    str_2.chars.delete_at(str_2.index(char))
-  end
-  true
-end
-
-# p second_anagram?("gizmo", "sally")    #=> false
-# p second_anagram?("elvis", "lives")    #=> true
+# arr_1 = [5, 3, -7] # 8
+# arr_2 = [2, 3, -6, 7, -6, 7, -9, -2, 5, 10] # 15
+# arr_3 = [-5, -1, -3] # -1
+# p largest_contiguous_subsum_2(arr_1)
+# p largest_contiguous_subsum_2(arr_2)
+# p largest_contiguous_subsum_2(arr_3)
