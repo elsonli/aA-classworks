9f064553f1e0b30f02d8bd246f93efc62b2814f0
Elson Li <elson.li@berkeley.edu>
2019-11-16 15:40:58 -0800

    Finish some of part 04a of W5D2 classwork - sql zoo project

--- W5D2/sql_zoo_project/lib/04a_countries_bonus.rb
+++ W5D2/sql_zoo_project/lib/04a_countries_bonus.rb
@@ -17,6 +17,13 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT countries.name
+    FROM countries
+    WHERE countries.gdp > (
+      SELECT MAX(countries.gdp)
+      FROM countries
+      WHERE countries.continent = 'Europe'
+    )
   SQL
 end
 
