5ee2915e48e3b68a12baee544207f9c4dc54c55d
Elson Li <elson.li@berkeley.edu>
2019-12-17 23:08:23 -0800

    Finish all phases except CSS and rendering before alert

--- W10D3/minesweeper_project/components/game.jsx
+++ W10D3/minesweeper_project/components/game.jsx
@@ -11,10 +11,21 @@ class Game extends React.Component {
     this.updateGame = this.updateGame.bind(this);
   }
 
-  updateGame() {
+  updateGame(tile, flagged) {
+    flagged ? tile.toggleFlag() : tile.explore();
+    this.setState({ board: this.state.board });
+  }
+
+  restartGame() {
+    const newBoard = new Minesweeper.Board(8, 10);
+    this.setState({ board: newBoard });
   }
 
   render() {
+    if (this.state.board.won() || this.state.board.lost()) {
+      alert("Game Over!");
+      this.restartGame();
+    }
     return (
       <div>
         <Board
