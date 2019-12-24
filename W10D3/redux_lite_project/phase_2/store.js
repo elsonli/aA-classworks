5477cfa86f752523e19d652fb60f46a0e7c03bcc
Elson Li <elson.li@berkeley.edu>
2019-12-23 21:55:42 -0800

    Finish W10D3 classwork - redux lite project phase 2

--- W10D3/redux_lite_project/phase_2/store.js
+++ W10D3/redux_lite_project/phase_2/store.js
@@ -10,7 +10,7 @@ class Store {
   }
 }
 
-// Phase 2 - Run in Chrome console
+// Phase 2 - Reducers and Actions (Run in Chrome console)
 let state = {
   user: "Ronil",
   role: "Student"
@@ -56,7 +56,63 @@ const roleReducer = (prevState, action) => {
   }
 }
 
-console.log(userReducer(state, userAction)); // => {user: "Ronil Bhatia", role: "Student"}
-console.log(roleReducer(state, roleAction)); // => {user: "Ronil", role: "Instructor"}
-console.log(userReducer(state, userRoleAction)) // => {user: "Ronil", role: "Student"}
-console.log(roleReducer(state, userRoleAction)) // => {user: "Ronil", role: "Student"}
+console.log(userReducer(state, userAction)); // => { user: "Ronil Bhatia", role: "Student" }
+console.log(roleReducer(state, roleAction)); // => { user: "Ronil", role: "Instructor" }
+console.log(userReducer(state, userRoleAction)) // => { user: "Ronil", role: "Student" }
+console.log(roleReducer(state, userRoleAction)) // => { user: "Ronil", role: "Student" }
+
+// Phase 2 - Combining Reducers (Run in Chrome console)
+const combineReducers = (reducersObj) => (prevState, action) => {
+  Object.freeze(prevState);
+  const newState = Object.assign({}, prevState);
+  const prevStateKeys = Object.keys(prevState);
+  for (const key of prevStateKeys) {
+    const oldValue = prevState[key];
+    const newValue = reducersObj[key](oldValue, action);
+    newState[key] = newValue;
+  }
+  return newState;
+}
+
+const myNoiseReducer = (prevState = "peace and quiet", action) => {
+  switch (action.type) {
+    case "noisy action":
+      return action.noise;
+    default:
+      return prevState;
+  }
+};
+
+const myNoisyAction = {
+  type: "noisy action",
+  noise: "Car alarm"
+};
+
+const myInconsequentialAction = {
+  type: "a type no one cares about",
+  data: {
+    thisThing: "will not get used anyway"
+  }
+};
+
+const myInitialState = {
+  noise: "peace and quiet"
+};
+
+const myRootReducer = combineReducers({
+  noise: myNoiseReducer,
+});
+
+// The first time you call `myRootReducer` with `myInconsequentialAction`, it
+// returns the initial state. This is because the only reducer, `myNoiseReducer`,
+// doesn't respond to that action type. When we invoke it with `newState` and
+// `myNoisyAction`, however, the `noise` key is modified because `myNoiseReducer`
+// returns something other than its `prevState` for the `"noisy action"` action
+// type. When we invoke the `rootReducer`, with `myInconsequentialAction` the
+// second time, the `noise` property doesn't revert back to it's default value,
+// it just doesn't change.
+let newState = myRootReducer(myInitialState, myInconsequentialAction);
+console.log(newState); // => { noise: "peace and quiet" }
+newState = myRootReducer(newState, myNoisyAction);
+console.log(newState); // => { noise: "Car alarm" }
+myRootReducer(newState, myInconsequentialAction); // => { noise: "Car alarm" }
