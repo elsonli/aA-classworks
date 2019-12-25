bba3f8b85b89ac6be05ad601ddbe743f9a453288
Elson Li <elson.li@berkeley.edu>
2019-12-24 22:06:19 -0800

    Finish Phase 1: Frontend Structure

---
+++ W10D4/todos_project_v2/frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Document Ready Callback
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>React Not Ded</h1>, root);
+});
