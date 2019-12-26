bf147c05127ea8bf190eed022a84e7dea5ad29a9
Elson Li <elson.li@berkeley.edu>
2019-12-26 03:42:03 -0800

    Finish Phase 1: Rails API

---
+++ W10D4/todos_project_v2_react_redux_rails/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+todo_1 = Todo.create(title: "Cheer Ran", body: "Afterglow", done: true)
+todo_2 = Todo.create(title: "Cheer Yukina", body: "Roselia", done: false)
+todo_3 = Todo.create(title: "Cheer Hina", body: "Pastel Palette", done: false)
