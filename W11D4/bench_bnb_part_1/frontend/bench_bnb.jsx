b296fb704e27ede13600b5c82c8a59313301710d
Elson Li <elson.li@berkeley.edu>
2020-01-02 23:24:34 -0800

    Finish Phase 1: Front-End User Authentication

--- W11D4/bench_bnb_part_1/frontend/bench_bnb.jsx
+++ W11D4/bench_bnb_part_1/frontend/bench_bnb.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as SessionAPIUtil from "./util/session_api_util";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch; // just for testing!
   ReactDOM.render(<h1>React Working</h1>, root);
 });
